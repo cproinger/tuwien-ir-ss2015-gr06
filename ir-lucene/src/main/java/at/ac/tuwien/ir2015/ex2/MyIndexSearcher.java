e5e5188d33d1a69b261525062521620ab95c6ac6
Kathi Schleidt <e8527180@student.tuwien.ac.at>
2015-05-09 18:20:08 +0200

    Again fixed MyIndexSearcher to provide proper output

--- ir-lucene/src/main/java/at/ac/tuwien/ir2015/ex2/MyIndexSearcher.java
+++ ir-lucene/src/main/java/at/ac/tuwien/ir2015/ex2/MyIndexSearcher.java
@@ -2,11 +2,15 @@ package at.ac.tuwien.ir2015.ex2;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Field;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
@@ -16,6 +20,8 @@ import org.apache.lucene.search.Sort;
 import org.apache.lucene.search.TopFieldDocs;
 import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.store.FSDirectory;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 import at.ac.tuwien.ir2015.ex2.similarity.BM25LSimilarity;
 
@@ -33,6 +39,14 @@ public class MyIndexSearcher {
 	}
 
 	public void search() throws IOException, ParseException {
+		
+		List<IndexableField> fields;
+		Field f;
+		String patternString = ".*<path:(.*)>";
+		String fileName = "";
+		int rank = 1;
+		float score = 0.0f;
+		
 		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths
 				.get(indexPath)));
 		IndexSearcher searcher = new IndexSearcher(reader);
@@ -47,11 +61,27 @@ public class MyIndexSearcher {
 		Query query = parser.parse(queryText);
 //		Query query = parser.parse("search for atheism");
 		TopFieldDocs result = searcher.search(query, 10, Sort.RELEVANCE);
+		Pattern pattern = Pattern.compile(patternString);
 		for(ScoreDoc d : result.scoreDocs) {
-			System.out.println(d);
-			System.out.println(d.score);
+			Iterator myIt; 
+			
+			fields = searcher.doc(d.doc).getFields();
+			myIt = fields.iterator();
+			while (myIt.hasNext()) {
+				f = (Field) myIt.next();
+		        Matcher matcher = pattern.matcher(f.toString());
+		        while(matcher.find()) {
+		            fileName = matcher.group(1);
+		        }
+			}
+			score = searcher.explain(query, d.doc).getValue();
+			System.out.println("TopicX Q0 " + fileName + " " + rank++ + " " + score + " group6-experiment1");
 
-			System.out.println(searcher.explain(query, d.doc));
+			//System.out.println(d);
+			//System.out.println(d.score);
+
+			//System.out.println(searcher.explain(query, d.doc));
 		}
 	}
 }
+
